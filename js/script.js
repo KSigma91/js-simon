beb83f311caf9c84fa97980e9e2b058c144e7248
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-17 20:44:33 +0200

    test 2

--- js/script.js
+++ js/script.js
@@ -9,24 +9,38 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 var numeriGioco = document.getElementById("mio_id");
 const tastoGenera = document.querySelector("button");
 
-
-
 // genero 5 numeri in maniera randomica
 for (let i = 0; i < 5; i++){
     generatoreNumeri(numeriGioco);    
 }
 
 // i numeri verranno mostrati al giocatore e da lì partirà un timer di 30 secondi
+let contoRovescia = 3;
 
+const conteggio = setInterval(
+    function() {
+        numeriGioco.innerHTML = contoRovescia;
+        const conteggioVis = document.createElement("h1");
+        conteggioVis.append(contoRovescia);
+        numeriGioco.innerHTML = contoRovescia;
+    }
+    , 1000);
 
 
 
 // dopo 30 secondi il giocatore dovrà inserire i 5 numeri generati nel campo apposito (prompt)
-const clock = setTimeout(myFunzione, 3000);
-
-function myFunzione() {
-    numeriGioco.innerHTML = "";
-}
+const clock = setTimeout(
+    function() {
+        numeriGioco.innerHTML = "";
+        let indovinaNumeri;
+        if(conteggio === 0){
+            clearInterval(counting);
+            indovinaNumeri = parseInt(prompt("Quali sono i numeri?"));
+        } else {
+            contoRovescia = contoRovescia - 1;
+        }
+    }
+    , 5000);
 
 // alla fine della partita il software dirà quanti e quali numeri sono stati indovinati
 
